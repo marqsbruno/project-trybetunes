c56ba6747ad1e9713281a34debbcf20b54001777
bruno <marqsbruno@gmail.com>
2022-03-07 18:22:24 -0300

    req 2 feito com gambiarras

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nameUser: '',
+      loading: false,
+      redirect: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      nameUser: target.value,
+    });
+  }
+
+  // then para esperar a promise e setar o valor do loading novo state para direcionar (gambiarra);
+  handleClick = () => {
+    const { nameUser: user } = this.state;
+    this.setState({ loading: true });
+    createUser({ name: user }).then(() => {
+      this.setState({
+        loading: false,
+        redirect: true,
+      });
+    });
+    console.log(user);
+  }
+
   render() {
+    const { nameUser: userName, loading, redirect } = this.state;
+    const minChar = 3; // minimo de caracteres;
     return (
       <div data-testid="page-login">
-        <p>testando</p>
+        { loading ? <p>Carregando...</p>
+          : (
+            <form>
+              <input
+                data-testid="login-name-input"
+                id="input-name"
+                onChange={ this.handleChange }
+              />
+              <button
+                data-testid="login-submit-button"
+                type="button"
+                disabled={ userName.length < minChar }
+                onClick={ this.handleClick }
+              >
+                Entrar
+              </button>
+            </form>
+          )}
+        {redirect ? <Redirect to="/search" /> : ''}
       </div>
     );
   }
