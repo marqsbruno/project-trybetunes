762f4ccf97fe295b46c8e9f3250fb8a7b5a0ad04
bruno <marqsbruno@gmail.com>
2022-03-12 13:42:11 -0300

    req 6 final

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class AlbumCard extends React.Component {
   render() {
@@ -28,5 +29,14 @@ class AlbumCard extends React.Component {
     );
   }
 }
+
+AlbumCard.propTypes = {
+  data: PropTypes.shape({
+    artistName: PropTypes.string,
+    collectionId: PropTypes.number,
+    collectionName: PropTypes.string,
+    artworkUrl100: PropTypes.string,
+  }).isRequired,
+};
 
 export default AlbumCard;
